d876aa94814db379a6d5f689830ca5585e1ba8fb
KashyapRadadiya <kashyapradadiya1234@gmail.com>
2025-09-24 16:30:47 +0530

    testfile updated

--- test/test_app.js
+++ test/test_app.js
@@ -6,6 +6,6 @@ describe('GET /', () => {
   it('responds with JSON message', async () => {
     const res = await request(app).get('/');
     expect(res.status).to.equal(200);
-    expect(res.body).to.have.property('message', 'Hello from Jenkins CI/CD');
+    expect(res.body).to.have.property('message', 'Hello from Jenkins CI/CD by kashyap');
   });
 });
